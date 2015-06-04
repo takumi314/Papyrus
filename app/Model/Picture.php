f67800873054e530c62bf3f3b8796e76fa9a1a25
takumi314 <isospin_symmetry@me.com>
2015-06-04 18:48:20 +0800

    各テーブルに対応するModelファイルを作成した。 まだ、ソースコードは書いていない。

---
+++ app/Model/Picture.php
@@ -0,0 +1,13 @@
+<?php
+class Picture extends AppModel {
+
+	public $belongsTo = 'Category';		// Categoryモデルからカテゴリーのデータを参照する。（アソシエーション）
+
+	public function isOwnedBy($post, $user) {
+        return $this->field('id', array('id' => $post, 'user_id' => $user)) !== false;
+    }
+
+
+}
+
+?>
