252baa3dec5b4d212695baf19b757a64a40ea71b
takumi314 <isospin_symmetry@me.com>
2015-06-05 14:56:02 +0800

    Afterlookモデルを新規で追加。すでにテーブル名afterlookも作成済。

---
+++ app/Model/Afterlook.php
@@ -0,0 +1,14 @@
+<?php
+
+App::uses('AppModel', 'Model');
+// App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');               // lib内のクラスをインクルードする。
+
+
+// app/Model/Afterlook.php
+class Afterlook extends AppModel {
+    
+
+}
+
+
+?>
